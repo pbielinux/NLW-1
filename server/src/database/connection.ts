4099f0d0a1c6bf7f65282961685418e68068b192
Pedro henrique Bielik <pbielik@c2r6s4.42lisboa.com>
2021-07-09 17:06:59 +0100

    SQLite Database connection

---
+++ server/src/database/connection.ts
@@ -0,0 +1,10 @@
+import knex from 'knex';
+import path from 'path';  // Node library to deal with paths, OS independent
+
+// Does the connection with the database
+const connection = knex({
+	client: 'sqlite3',   // The SQL client
+	connection: {
+		filename: path.resolve(__dirname, 'database.sqlite')		// The path of database file
+	}
+});
