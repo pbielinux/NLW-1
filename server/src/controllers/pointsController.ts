057f927436afd30ac0b9758470dead00860c75f2
Pedro henrique Bielik <pbielik@c2r6s4.42lisboa.com>
2021-07-13 20:11:51 +0100

    Routes: Create PointsController route

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -1,3 +1,41 @@
-class PointsController {};
+import { Request, Response } from 'express';
+import knex from "../database/connection";
+
+class PointsController {
+	async create(request: Request, response: Response) {
+		const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
+
+		// Create knex transaction, if the first query fail the second doesn't execute
+		const trx = await knex.transaction();
+
+		const point = {
+			image: 'image-fake',
+			name,
+			email,
+			whatsapp,
+			latitude,
+			longitude,
+			city,
+			uf
+		}
+
+		const insertedIds = await trx('points').insert(point);
+
+		const point_id = insertedIds[0];
+		const pointItems = items.map((item_id: number) => {
+			return {
+				item_id,
+				point_id
+			}
+		});
+		// Relationship with Items Tables
+		await trx('point_items').insert(pointItems);
+
+		return response.json({
+			id: point_id,
+			...point
+		});
+	};
+};
 
 export default PointsController;
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import knex from './database/connection';
 
+import PointsController from './controllers/pointsController';
+
 const routes = express.Router();
+const pointsController = new PointsController();
 
 // Route to list all Items
 routes.get('/items', async (request, response) => {
@@ -19,47 +22,6 @@ routes.get('/items', async (request, response) => {
 	return response.json(serializedItems);
 });
 
-// Rout to create collect point, so... POST!
-routes.post('/points', async (request, response) => {
-	// Disruption JS, when you kow the fields, same as:
-	// const name = request.body.name;
-	// const email = request.body.email; ...
-	const {
-		name,
-		email,
-		whatsapp,
-		latitude,
-		longitude,
-		city,
-		uf,
-		items
-	} = request.body;
-
-	// Create knex transaction, if the first query fail the second doesn't execute
-	const trx = await knex.transaction();
-
-	const insertedIds = await trx('points').insert({		// The knex insert() method return the IDS of inserted registers
-		image: 'image-fake',
-		name,	// When the value == variable name can be the short way
-		email,
-		whatsapp,
-		latitude,
-		longitude,
-		city,
-		uf
-	});
-
-	const point_id = insertedIds[0];
-	const pointItems = items.map((item_id: Number) => {
-		return {
-			item_id,
-			point_id
-		}
-	});
-	// Relationship with Items Tables
-	await trx('point_items').insert(pointItems);
-
-	return response.json({ success: true })
-});
+routes.post('/points', pointsController.create);
 
 export default routes;
